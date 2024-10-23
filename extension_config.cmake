e1da538ce1fe9fe01dc80872c99da40a8d043bea
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-10-23 20:36:45 +0200

    Skip tests on MinGW

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,7 +1,16 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
+################# HTTPFS
+# Windows MinGW tests for httpfs currently not working
+if (NOT MINGW)
+    set(LOAD_HTTPFS_TESTS "LOAD_TESTS")
+else ()
+    set(LOAD_HTTPFS_TESTS "")
+endif()
+
 duckdb_extension_load(httpfs
 	DONT_LINK
 	SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
 	INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/httpfs/include
-	LOAD_TESTS)
+	${LOAD_HTTPFS_TESTS}
+)
