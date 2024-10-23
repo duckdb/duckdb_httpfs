81ef6aafd13079e73145d8fa8fffc5f2a2811c0a
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-10-23 14:53:44 +0200

    Avoid linking, remove parquet includes

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,6 +1,7 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
 duckdb_extension_load(httpfs
+	DONT_LINK
 	SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
 	INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/httpfs/include
 	LOAD_TESTS)
